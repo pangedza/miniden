53c8b50defa6b6c3c2407d10ed6fdf0893e6b497
pangedza <fatal47887@gmail.com>
2025-11-08 20:54:51 +0300

    feat(theme): add TT5 child theme with unified global style and animations

---
+++ wp-content/themes/tt5-child/functions.php
@@ -0,0 +1,8 @@
+<?php
+add_action('wp_enqueue_scripts', function(){
+  wp_enqueue_style('tt5-child-style', get_stylesheet_uri(), [], '1.0');
+  wp_enqueue_style('tt5-child-custom', get_stylesheet_directory_uri().'/assets/css/custom.css', ['tt5-child-style'], '1.0');
+});
+add_action('after_setup_theme', function(){
+  add_theme_support('woocommerce');
+});
